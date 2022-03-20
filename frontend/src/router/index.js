96665f3f110544d696138bffbba4f0aaecb01cd5
it62070120 <62070120@it.kmitl.ac.th>
2022-03-21 00:28:33 +0700

    feat: router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,38 +3,63 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routers = [
+const routes = [
   {
     path: '/',
     name: 'home',
-    component: () => import('../views/home.vue'),
-  },
-  {
-    path: '/store/managebook/:storeId',
-    name: 'manageStore',
-    component: () => import('../views/manageBook.vue')
-  },
-  {
-    path: '/store/editbook/:bookId',
-    name: 'editBook',
-    component: () => import('../views/editBook.vue')
-  },
-  {
-    path: '/store/addbook/:storeId',
-    name: 'addBook',
-    component: () => import('../views/addBook.vue')
+    component: () => import('../views/Home.vue'),
   },
   {
     path: '/account/login',
     name: 'login',
+    meta: { guess: true },
     component: () => import('../views/account/login.vue')
   },
+  {
+    path: '/account/register',
+    name: 'register',
+    meta: { guess: true },
+    component: () => import('../views/account/register.vue')
+  },
+  {
+    path: '/store/managebook/:storeId',
+    name: 'manageStore',
+    component: () => import('../views/store/managebook.vue')
+  },
+  {
+    path: '/store/editbook/:bookId',
+    name: 'editBook',
+
+    component: () => import('../views/store/editbook.vue')
+  },
+  {
+    path: '/store/addbook/:storeId',
+    name: 'addBook',
+
+    component: () => import('../views/store/addBook.vue')
+  }
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routers
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  const isLoggedIn = !!localStorage.getItem('token')
+
+  if (to.meta.login && !isLoggedIn) {
+    alert('You are a guess user. Please login to use this function.')
+    next({ path: '/' })
+  }
+
+  if (to.meta.guess && isLoggedIn) {
+    alert("You've already logged in.")
+    next({ path: '/'})
+  }
+
+  next()
 })
 
 export default router
