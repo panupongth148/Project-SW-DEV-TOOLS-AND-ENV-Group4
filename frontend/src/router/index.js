215eede969270dcf0f49e2bc5faccfe3be525285
it62070120 <62070120@it.kmitl.ac.th>
2022-03-21 00:26:56 +0700

    Revert "feat: router"
    This reverts commit ef9746b4e4f61fd81defd7b39054ffc7010cc491.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,63 +3,38 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [
+const routers = [
   {
     path: '/',
     name: 'home',
-    component: () => import('../views/Home.vue'),
-  },
-  {
-    path: '/account/login',
-    name: 'login',
-    meta: { guess: true },
-    component: () => import('../views/account/login.vue')
-  },
-  {
-    path: '/account/register',
-    name: 'register',
-    meta: { guess: true },
-    component: () => import('../views/account/register.vue')
+    component: () => import('../views/home.vue'),
   },
   {
     path: '/store/managebook/:storeId',
     name: 'manageStore',
-    component: () => import('../views/store/managebook.vue')
+    component: () => import('../views/manageBook.vue')
   },
   {
     path: '/store/editbook/:bookId',
     name: 'editBook',
-
-    component: () => import('../views/store/editbook.vue')
+    component: () => import('../views/editBook.vue')
   },
   {
     path: '/store/addbook/:storeId',
     name: 'addBook',
-
-    component: () => import('../views/store/addBook.vue')
-  }
+    component: () => import('../views/addBook.vue')
+  },
+  {
+    path: '/account/login',
+    name: 'login',
+    component: () => import('../views/account/login.vue')
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
-})
-
-router.beforeEach((to, from, next) => {
-  const isLoggedIn = !!localStorage.getItem('token')
-
-  if (to.meta.login && !isLoggedIn) {
-    alert('You are a guess user. Please login to use this function.')
-    next({ path: '/' })
-  }
-
-  if (to.meta.guess && isLoggedIn) {
-    alert("You've already logged in.")
-    next({ path: '/'})
-  }
-
-  next()
+  routers
 })
 
 export default router
