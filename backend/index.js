2d473c916669df6a14bab5ba3b3ae4337857c33f
it62070042 <56545080+fchana@users.noreply.github.com>
2022-03-20 20:05:18 +0700

    feat: apply user router

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,10 @@
 const express = require("express")
 
-const functions = require('firebase-functions');
-const builderFunction = functions.region('asia-east2').https;
 const app = express();
 const cors = require('cors')
 const { logger } = require('./middlewares')
 const bp = require('body-parser')
-const config = require('./config')
+const port=process.env.PORT || 3000;
 
 app.use(cors())
 app.use(logger)
@@ -19,24 +17,16 @@ app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 // routers
-
 const bookRouter = require('./routes/book')
-
-
 const storeRouter = require('./routes/store')
-
-
-
+const userRouter = require('./routes/user')
 
 app.use(bookRouter.router)
-
-
+// app.use(commentRouter.router)
+// app.use(imageRouter.router)
 app.use(storeRouter.router)
+app.use(userRouter.router)
 
-
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`Example app listening at http://localhost:3000`)
 })
-
-
-// exports.hello = builderFunction.onRequest(sample.hello);
