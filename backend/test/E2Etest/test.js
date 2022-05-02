ca8d7ed42caa50129ca0acb2f570b881002a0151
panupongth148 <97428822+panupongth148@users.noreply.github.com>
2022-05-02 13:37:22 +0700

    Update test.js

--- backend/test/E2Etest/test.js
+++ backend/test/E2Etest/test.js
@@ -36,50 +36,51 @@ describe('Task Api', async () => {
                 // .attach('myImage', fs.readFileSync('./static/uploads/testimage.jpg'))
                 .end((err, response) => {
                     response.should.have.status(200);
+                    idShouldUpdate = response.bookid
                     // response.body.should.be.a('array');
                     
                 })
         })
     //update
-       await it('It should be updatebook', async () => {
-            const snapshot = await firebase
-                .firestore()
-                .collection("book")
-                .where("store_id", "==", "999")
-                .get();
+    //    await it('It should be updatebook', async () => {
+    //         // const snapshot = await firebase
+    //         //     .firestore()
+    //         //     .collection("book")
+    //         //     .where("store_id", "==", "999")
+    //         //     .get();
 
-                // console.log("id firebase : " + snapshot.length)
-            await snapshot.forEach((res) => {
-                // idcollection = res.id;
-                // console.log(res.data())
-                if(res.data().book_name == "test"){
-                    idShouldUpdate = res.id
-                }
+    //         //     // console.log("id firebase : " + snapshot.length)
+    //         // await snapshot.forEach((res) => {
+    //         //     // idcollection = res.id;
+    //         //     // console.log(res.data())
+    //         //     if(res.data().book_name == "test"){
+    //         //         idShouldUpdate = res.id
+    //         //     }
 
-
-            });
-            // console.log("id firebase : " + idShouldUpdate)
-            chai.request(server).put(`/store/editbook/${idShouldUpdate}`)
-                .set('Content-Type', 'multipart/form-data')
-                .field({
-                    book_name: "test-update",
-                    book_type: "book",
-                    book_count: "1",
-                    book_price: "200",
-                    book_category: "แปล",
-                    book_discount: 0,
-                    book_description: "adsssssdsa",
-                    store_id: 1
-                })
-                .attach('myImage', path.resolve('./static/uploads/', 'testimage.jpg'))
-                // .attach('myImage', fs.readFileSync('./static/uploads/testimage.jpg'))
-                .end((err, response) => {
-                    response.should.have.status(200);
+                
+    //         // });
+    //         console.log("id firebase : " + idShouldUpdate)
+    //         chai.request(server).put(`/store/editbook/${idShouldUpdate}`)
+    //             .set('Content-Type', 'multipart/form-data')
+    //             .field({
+    //                 book_name: "test-update",
+    //                 book_type: "book",
+    //                 book_count: "1",
+    //                 book_price: "200",
+    //                 book_category: "แปล",
+    //                 book_discount: 0,
+    //                 book_description: "adsssssdsa",
+    //                 store_id: 1
+    //             })
+    //             .attach('myImage', path.resolve('./static/uploads/', 'testimage.jpg'))
+    //             // .attach('myImage', fs.readFileSync('./static/uploads/testimage.jpg'))
+    //             .end((err, response) => {
+    //                 response.should.have.status(200);
                     
-                    // response.body.should.be.a('array');
+    //                 // response.body.should.be.a('array');
                     
-                })
-        })
+    //             })
+    //     })
 
     //  delete book api test
         await it('It should deletebook', async () => {
